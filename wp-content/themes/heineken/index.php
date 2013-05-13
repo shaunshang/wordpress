d4c07fb38dee0810fccbc74927915cf9a60434b3
Shaun Shang <shaun.shang@tbwa.com>
2013-05-13 12:17:02 +0800

    main body finished

--- wp-content/themes/heineken/index.php
+++ wp-content/themes/heineken/index.php
@@ -23,13 +23,15 @@
 					</div>
 					<div class="content-list">
 						<?php if(have_posts()) : ?>
-							<h3>
-								<a href="<?php the_permalink() ?>">
-									<?php the_title() ?>
-								</a>
-							</h3>
-						<?php else : ?>
-							
+							<?php while(have_posts()) : the_post() ?>
+								<h2>
+									<a href="<?php the_permalink() ?>"
+										title="Permanent link to <?php the_title_attribute() ?>">
+										<?php the_title() ?>
+									</a>
+								</h2>
+								<?php the_content() ?>
+							<?php endwhile; ?>
 						<?php endif ?>
 					</div>
 				</div>
